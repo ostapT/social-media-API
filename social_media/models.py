8146ee3f55cde3fb8fae63db54912bcfdc1a37a3
ostapix@gmail.com <@Bandera345>
2023-04-21 15:19:02 +0300

    Create Tag and Post models

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+
+def post_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/posts/", filename)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to=post_image_file_path)
